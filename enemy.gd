ad2034c6693ab18bb4bb1f138a2d2416678028dd
zuzu0103 <zuzuscamp@gmail.com>
2025-03-30 11:27:12 +0100

    Add bullets
    - Added bullet scene with sprite
    - Causes gun to shoot bullets periodically

--- enemy.gd
+++ enemy.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
 @onready  var player = get_node("/root/Game/Player")
-var speed = 15000.0
+var speed = 300.0
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * speed * delta
+	velocity = direction * speed
 	move_and_slide()
--- gun.gd
+++ gun.gd
@@ -5,3 +5,14 @@ func _physics_process(delta):
 	if enemies_in_range.size() > 0:
 		var target_enemy = enemies_in_range.front()
 		look_at(target_enemy.global_position)
+
+func shoot():
+	const BULLET = preload("res://bullet.tscn")
+	var new_bullet = BULLET.instantiate()
+	new_bullet.global_position = %ShootingPoint.global_position
+	new_bullet.global_rotation = %ShootingPoint.global_rotation
+	%ShootingPoint.add_child(new_bullet)
+
+
+func _on_timer_timeout() -> void:
+	shoot()
